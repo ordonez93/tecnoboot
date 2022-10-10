0be340c9a49c581a4986caf407b59070706fac18
Andres <you@example.com>
2022-10-10 14:50:06 -0500

    modelos servicios fin

--- tecnoboot/Aplicaciones/solicitud_servicios/admin.py
+++ tecnoboot/Aplicaciones/solicitud_servicios/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import clientes, servicios, solicitud, cobros
 # Register your models here.
+admin.site.register(clientes)
+admin.site.register(servicios)
+admin.site.register(solicitud)
+admin.site.register(cobros)
--- tecnoboot/Aplicaciones/solicitud_servicios/models.py
+++ tecnoboot/Aplicaciones/solicitud_servicios/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # modelos de solicitud de servicios.
+
+class clientes(models.Model):
+    id=models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    ciudad = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=80)
+    telefono = models.CharField(max_length=20)
+    
+
+    def __str__(self):
+        return self.nombre
+
+class servicios(models.Model):
+    id=models.AutoField(primary_key=True)
+    servicio = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=100)
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return self.servicio
+
+class solicitud(models.Model):
+    id=models.AutoField(primary_key=True)
+    cliente = models.ForeignKey(clientes, on_delete=models.CASCADE)
+    servicio = models.ForeignKey(servicios, on_delete=models.CASCADE)
+    sfechahora = models.DateTimeField(auto_now_add=True)
+    estado = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.estado
+
+class cobros(models.Model):
+    id=models.AutoField(primary_key=True)
+    solicitud = models.ForeignKey(solicitud, on_delete=models.CASCADE)
+    cfechahora = models.DateTimeField(auto_now_add=True)
+    valorcobrado = models.IntegerField()
+
+    def __str__(self):
+        return self.valor
