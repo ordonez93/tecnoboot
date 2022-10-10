6627e39629f5881cb9de9fce724a639ecd2b1028
Andres <you@example.com>
2022-10-10 18:03:43 -0500

    listando servicios en el template

--- tecnoboot/Aplicaciones/solicitud_servicios/views.py
+++ tecnoboot/Aplicaciones/solicitud_servicios/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template, context
 from django.template.loader import get_template
-
+from .models import clientes, servicios, solicitud, cobros
 # vistas para la aplicacion solicitud_servicios
 
 def inicio(request):
-    
-    return render(request,"inicio.html")
+    listaServicios = servicios.objects.all()
+    return render(request,"inicio.html",{"servicios":listaServicios})
 
-def nuevo_servicio(request):
-    pass
