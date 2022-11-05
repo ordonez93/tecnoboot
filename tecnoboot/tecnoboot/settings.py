924323dc93454a2b444b64a02385c31adb5c9dca
Andres <you@example.com>
2022-11-04 23:01:09 -0500

    solucion static

--- tecnoboot/tecnoboot/settings.py
+++ tecnoboot/tecnoboot/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'tecnoboot/static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'Aplicaciones/solicitud_servicios/static/'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
