4ea71eed023fcd83557d5f9cdd25b5489e67bb7c
Andres <you@example.com>
2022-11-19 00:37:50 -0500

    api-whatsapp

--- tecnoboot/Aplicaciones/solicitud_servicios/views.py
+++ tecnoboot/Aplicaciones/solicitud_servicios/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse
 from django.template import Template, context
 from django.template.loader import get_template
 from .models import clientes, servicios, solicitud
+import datetime
 # vistas para la aplicacion solicitud_servicios
 
 def inicio(request):
     listaServicios = servicios.objects.all()
-    return render(request,"inicio.html",{"servicios":listaServicios})
+    fecha_now = datetime.datetime.now()
+    return render(request,"inicio.html",{"servicios":listaServicios,"fecha": fecha_now.year})
 
 def solicitarServicio(request):
     if request.method == "POST":
